bf3d2c3eecf019739eb446f62f27e7b1988181a8
bittlingmayer <adam+code@signaln.com>
2017-09-07 15:23:46 +0400

    Fix - rm .py from module name

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
   name = 'parallelio',
-  pymodules = ['parallelio.py'],
-  version = '0.5',
+  pymodules = ['parallelio'],
+  version = '0.6',
   description = 'Basic tools for working with natural language text data',
   author = 'Signal N',
   author_email = 'parallelio@signaln.com',
   url = 'https://github.com/signaln/parallelio',
-  download_url = 'https://github.com/signaln/parallelio/archive/0.5.tar.gz',
+  download_url = 'https://github.com/signaln/parallelio/archive/0.6.tar.gz',
   keywords = ['text', 'data', 'files', 'io', 'natural language processing'],
   classifiers = [],
 )
