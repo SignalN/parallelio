d187d7aba2e87a93e0e64d24f9a508a26d7b9e48
bittlingmayer <adam+code@signaln.com>
2017-09-07 17:43:40 +0400

    Add long description, classifiers

--- setup.py
+++ setup.py
@@ -3,12 +3,20 @@ from setuptools import setup
 setup(
   name = 'parallelio',
   packages = ['parallelio'],
-  version = '0.8',
+  version = '0.9',
   description = 'Basic tools for working with natural language text data',
   author = 'Signal N',
   author_email = 'parallelio@signaln.com',
   url = 'https://github.com/signaln/parallelio',
-  download_url = 'https://github.com/signaln/parallelio/archive/0.8.tar.gz',
+  download_url = 'https://github.com/signaln/parallelio/archive/0.9.tar.gz',
   keywords = ['text', 'data', 'files', 'io', 'natural language processing'],
-  classifiers = [],
+  license='MIT',
+  classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
