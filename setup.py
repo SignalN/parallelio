b43a7bea2e28e24b3dfacdb51cbd9b61edd03a6f
bittlingmayer <adam+code@signaln.com>
2017-09-07 15:13:51 +0400

    New push script

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'parallelio',
   pymodules = ['parallelio.py'],
-  version = '0.1',
+  version = '0.4',
   description = 'Basic tools for working with natural language text data',
   author = 'Signal N',
   author_email = 'parallelio@signaln.com',
-  url = 'https://github.com/signaln/language',
-  download_url = 'https://github.com/signaln/parallelio/archive/0.1.tar.gz',
+  url = 'https://github.com/signaln/parallelio',
+  download_url = 'https://github.com/signaln/parallelio/archive/0.4.tar.gz',
   keywords = ['text', 'data', 'files', 'io', 'natural language processing'],
   classifiers = [],
 )
